cddc97e4da334028dc2edeaf5a191be404acab23
Seiji Sogabe <s.sogabe@gmail.com>
2013-09-27 09:59:26 +0900

    added No SCM, No Trigger cases,.

--- src/test/java/org/jenkinsci/plugins/gitbucket/GitBucketWebHookTest.java
+++ src/test/java/org/jenkinsci/plugins/gitbucket/GitBucketWebHookTest.java
@@ -12,10 +12,10 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.kohsuke.stapler.StaplerRequest;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Test for {@link GitBucketWebHook} class.
@@ -83,6 +83,57 @@ public class GitBucketWebHookTest {
         // make sure that onPost() never  called.
         verify(trigger, never()).onPost();
     }
+
+    @Test
+    public void testPushTrigger_NoTrigger() throws Exception {
+        // Repository URL
+        String repo = j.createTmpDir().getAbsolutePath();
+
+        // Setup FreeStyle Project
+        FreeStyleProject fsp = j.createFreeStyleProject("GitSCM Project");
+
+        // Setup Trigger(No Trigger)
+
+        // Setup SCM
+        SCM scm = new GitSCM(repo);
+        fsp.setScm(scm);
+
+        // Setup WebHook request
+        String payload = createPayload(repo);
+        StaplerRequest req = mock(StaplerRequest.class);
+        when(req.getParameter("payload")).thenReturn(payload);
+
+        // Post WebHook
+        GitBucketWebHook hook = new GitBucketWebHook();
+        hook.doIndex(req);
+    }
+    
+    @Test
+    public void testPushTrigger_NoSCM() throws Exception {
+        // Repository URL
+        String repo = j.createTmpDir().getAbsolutePath();
+
+        // Setup FreeStyle Project
+        FreeStyleProject fsp = j.createFreeStyleProject("GitSCM Project");
+
+        // Setup Trigger
+        GitBucketPushTrigger trigger = mock(GitBucketPushTrigger.class);
+        fsp.addTrigger(trigger);
+
+        // Setup SCM (No SCM)
+
+        // Setup WebHook request
+        String payload = createPayload(repo);
+        StaplerRequest req = mock(StaplerRequest.class);
+        when(req.getParameter("payload")).thenReturn(payload);
+
+        // Post WebHook
+        GitBucketWebHook hook = new GitBucketWebHook();
+        hook.doIndex(req);
+
+        // make sure that onPost() never  called.
+        verify(trigger, never()).onPost();
+    }
     
     @Test(expected = IllegalArgumentException.class)
     public void testPushTrigger_NoPayload() throws Exception {
@@ -139,7 +190,7 @@ public class GitBucketWebHookTest {
 
         verify(trigger, times(1)).onPost();
     }
-
+    
     private String createPayload(String url) {
         JSONObject repository = new JSONObject();
         repository.put("url", url);
